b936163ade275769706ee25acdc201b55bfd9d37
Michelle Luo <mluo0301@gmail.com>
2016-12-14 21:27:11 -0800

    update server session time

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -3,6 +3,7 @@ var router = express.Router();
 // var pwUtil = require('../helpers/password');
 var bcrypt = require('bcryptjs');
 var User = require('../models').User;
+var Truck = require('../models').Truck;
 
 //this is the users_controller.js file
 router.get('/signup-signin', function(req,res) {
@@ -122,5 +123,66 @@ router.get("/sign-out", function(req,res) {
      res.redirect("/")
   })
 });
+
+
+
+
+//test for sending request from USER SIDE
+
+router.get("/request-for-help", function(req,res) {
+    User.find({email: req.body.email}, function(err, users) {
+
+      var requestTime = new Date();
+      var requestId = new ObjectID; //Generate unique ID for the request
+
+
+
+      req.session.logged_in = true;
+      // the username to the session
+      req.session.username = user.username;
+      // and the user's email.
+      req.session.user_email = user.email;
+
+      req.session.firstName = user.firstName;
+
+      req.session.longitude = user.longitude;
+
+     req.session.latitude = user.latitude;
+
+
+
+
+
+});
+
+
+
+function fetchNearestTrucks(db, coordinates, callback) {
+
+    Truck.createIndex({"location": "2dsphere"}, function() {
+        Truck.find({
+                $near: {
+                    $geometry: {
+                        type: "Point",
+                        coordinates: longitude,latitude
+                    },
+                    $maxDistance: 2000
+                }
+            }
+        }).toArray(function(err, results) {
+            if (err) {
+                console.log(err)
+            } else {
+                callback(results);
+            }
+        });
+    });
+}
+
+
+
+
+
+
 
 module.exports = router;
