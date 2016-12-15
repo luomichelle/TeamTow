8ae701daa9f22a6285cceb9e5c3c2e1a6e94ebfb
Michelle Luo <mluo0301@gmail.com>
2016-12-15 11:13:37 -0800

    merge with master

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var bcrypt = require('bcryptjs');
 var User = require('../models').User;
 var Truck = require('../models').Truck;
+var RequestDB = require('../models').Request;
 
 //this is the users_controller.js file
 router.get('/signup-signin', function(req,res) {
@@ -126,6 +127,40 @@ router.get("/sign-out", function(req,res) {
 
 
 
+
+//============================router for reuqest
+router.post("/user-request", function(req, res) {
+    console.log("user-reuqest clicked!")
+  RequestDB.find({email: req.body.email}, function(err, users) {
+        console.log("user-reuqest clicked!1111")
+    if(err) throw err;
+    var user = users[0];
+      if(success) {
+       console.log("user-reuqest clicked!222222")
+
+        req.session.logged_in = true;
+        // the username to the session
+        req.session.username = user.username;
+        // and the user's email.
+        req.session.user_email = user.email;
+
+        req.session.firstName = user.firstName;
+
+        req.session.lastName = user.lastName;
+
+        res.render('index', {
+          email: req.session.user_email,
+          logged_in: req.session.logged_in,
+          username: req.session.username,
+          firstName: req.session.firstName,
+          lastName: req.session.lastName
+        });
+      } else {
+        req.session.logged_in = false;
+        res.redirect("index");
+      }
+    });
+  });
 
 
 
