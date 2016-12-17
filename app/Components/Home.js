b8a16cd7f0bba1777a8ba92e01217bed5bee6b07
Michelle Luo <mluo0301@gmail.com>
2016-12-17 07:22:24 -0800

    merge

--- app/Components/Home.js
+++ app/Components/Home.js
@@ -20,34 +20,7 @@ var Home = React.createClass({
 	},
 	render: function() {
 		return (
-				 
-<nav id="mainNav" class="navbar navbar-default navbar-fixed-top navbar-custom">
-        <div class="container">
-            <div class="navbar-header page-scroll">
-                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-                    <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
-                </button>
-                <a class="navbar-brand" href="/index">TeamTow</a>
-                <a class="ace-icon glyphicon glyphicon-road"></a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a className="btn btn-success btn-md truckbtn" id="become-host" href="/trucks/signup-signin">Become a Tow Truck Provider</a>
-                    </li>
-
-                    <li>
-                        <a className="page-scroll" href="/users/signup-signin">Sign In</a>
-                    </li>
-                    <li>
-                        <a href="/users/sign-out"><span className="glyphicon glyphicon-log-in"></span> Sign Out</a>
-                    </li>
-                </ul>
-            </div>
-        </div>
-    </nav>
-
+				 <p>hi</p>
 		);
 	}
 });
